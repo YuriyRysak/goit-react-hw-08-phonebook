3a02481202cd339ee555cb0830d95a3fe73b7a3c
YuriyRysak <yuriy.risak@gmail.com>
2023-03-23 13:21:18 +0200

    Change update file redux stire

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,4 @@
-import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 import {
   persistStore,
   persistReducer,
@@ -14,13 +14,7 @@ import { contactsReducer } from './contacts/contactsSlice';
 import { filterReducer } from './contacts/filterSlice';
 import { authReducer } from './auth/slice';
 
-const middleware = [
-  ...getDefaultMiddleware({
-    serializableCheck: {
-      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-    },
-  }),
-];
+
 
 // Persisting token field from auth slice to localstorage
 const authPersistConfig = {
@@ -35,8 +29,60 @@ export const store = configureStore({
     contacts: contactsReducer,
     filter: filterReducer,
   },
-  middleware,
-  devTools: process.env.NODE_ENV === 'development',
+  
+  middleware: getDefaultMiddleware => [
+    ...getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+  ],
+
+ 
 });
 
 export const persistor = persistStore(store);
+
+// --------------------------------------------------------------------------------------------
+// import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+// import {
+//   persistStore,
+//   persistReducer,
+//   FLUSH,
+//   REHYDRATE,
+//   PAUSE,
+//   PERSIST,
+//   PURGE,
+//   REGISTER,
+// } from 'redux-persist';
+// import storage from 'redux-persist/lib/storage';
+// import { contactsReducer } from './contacts/contactsSlice';
+// import { filterReducer } from './contacts/filterSlice';
+// import { authReducer } from './auth/slice';
+
+// const middleware = [
+//   ...getDefaultMiddleware({
+//     serializableCheck: {
+//       ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+//     },
+//   }),
+// ];
+
+// // Persisting token field from auth slice to localstorage
+// const authPersistConfig = {
+//   key: 'auth',
+//   storage,
+//   whitelist: ['token'],
+// };
+
+// export const store = configureStore({
+//   reducer: {
+//     auth: persistReducer(authPersistConfig, authReducer),
+//     contacts: contactsReducer,
+//     filter: filterReducer,
+//   },
+//   middleware,
+//   devTools: process.env.NODE_ENV === 'development',
+// });
+
+// export const persistor = persistStore(store);
